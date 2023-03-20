26a7e608c5ad0cc006f51d05656f53b280cb8823
ShaikhZuber08 <90678325+ShaikhJuber@users.noreply.github.com>
2023-03-20 09:57:34 +0530

    hide private api key

--- src/component/Map.js
+++ src/component/Map.js
@@ -42,7 +42,7 @@ const Map = (props) => {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=x7qY8C2t9lFFEzK67Ap0"
+          url={YOUR_MAP_API_KEY}
         />
         {userPlace.loaded && !userPlace.error && (
           <Marker
